16e3e50cc052cd0d13c72fbd9ea79c83b8f6df44
akshitajaiswal2908 <akshitajaiswal2908@gmail.com>
2025-07-29 19:49:40 +0530

    added auth in get products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
 const productController = require('../controllers/productController');
 
-router.get('/products' , productController.getAllProducts);
-router.get('/product/:id' , productController.getProductById)
+router.get('/products' , authMiddleware,  productController.getAllProducts);
+router.get('/product/:id' , authMiddleware, productController.getProductById)
 
 module.exports = router;
