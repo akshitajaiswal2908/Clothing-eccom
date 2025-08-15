5a098aa090a97ecb5560a4f425a6e21c3ef7ce82
akshitajaiswal2908 <akshitajaiswal2908@gmail.com>
2025-08-16 02:08:17 +0530

    minor

--- config/passport.js
+++ config/passport.js
@@ -30,29 +30,29 @@ passport.use(
   )
 );
 //facebook
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-    profileFields: ['id', 'displayName', 'emails', 'photos']
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      const email = profile.emails ? profile.emails[0].value : `${profile.id}@facebook.com`;
-      let user = await User.findOne({ where: { email } });
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FACEBOOK_APP_ID,
+//     clientSecret: process.env.FACEBOOK_APP_SECRET,
+//     callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+//     profileFields: ['id', 'displayName', 'emails', 'photos']
+//   },
+//   async (accessToken, refreshToken, profile, done) => {
+//     try {
+//       const email = profile.emails ? profile.emails[0].value : `${profile.id}@facebook.com`;
+//       let user = await User.findOne({ where: { email } });
 
-      if (!user) {
-        user = await User.create({
-          name: profile.displayName,
-          email
-        });
-      }
-      done(null, user);
-    } catch (err) {
-      done(err, null);
-    }
-  }
-));
+//       if (!user) {
+//         user = await User.create({
+//           name: profile.displayName,
+//           email
+//         });
+//       }
+//       done(null, user);
+//     } catch (err) {
+//       done(err, null);
+//     }
+//   }
+// ));
 
 
 passport.serializeUser((user, done) => done(null, user.user_id));
